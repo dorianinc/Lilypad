4ab2ebf4e5748fe48814100a6f97418381f29bfb
dorianinc <dorianrdba@gmail.com>
2023-03-15 14:55:42 -0700

    okay NOW phase 2 is completed

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -7,7 +7,9 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const { ValidationError } = require('sequelize');
 // initialize express
+const routes = require('./routes');
 const app = express();
 // connect to morgan middleware for logging info and requests/responses
 app.use(morgan('dev'));
@@ -15,7 +17,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
-// Security Middleware
+/////////////////////////// Start of Security Middleware /////////////////////////
 if (!isProduction) {
   // enable cors only in development
   app.use(cors());
@@ -38,12 +40,47 @@ app.use(
     }
   })
 );
-
-// backend/app.js
-const routes = require('./routes');
-
-// ...
+////////////////////////// End of Security Middleware /////////////////////////
 
 app.use(routes); // Connect all the routes
+
+////////////////////////// Start of Error Handlers /////////////////////////
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
+////////////////////////// End of Error Handlers /////////////////////////
 
 module.exports = app;
