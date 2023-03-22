a3fbc9e9131eac63b6b5ae9ae6cc49263f83df9e
dorianinc <dorianrdba@gmail.com>
2023-03-22 08:21:38 -0700

    succesfully completed get all spots,create new spot and add spot image

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,14 +1,52 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
 const { check } = require("express-validator");
-const { handleValidationErrors } = require("../../utils/validation");
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User } = require("../../db/models");
+const { validateSpotPost } = require("../../utils/validation");
+const { Spot, SpotImage } = require("../../db/models");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    
+  const spots = await Spot.findAll();
+  res.status(200).json(spots);
+});
+
+router.post("/", validateSpotPost, async (req, res) => {
+  const { user } = req;
+  const userId = user.dataValues.id;
+  const { address, city, state, country, name, description, price } = req.body;
+  if (user) return res.status(400).json(`Something went wrong!`);
+
+  await Spot.create({
+    address,
+    city,
+    state,
+    city,
+    country,
+    name,
+    description,
+    price,
+    ownerId: userId,
+  });
+  res.status(201);
+  res.json(`Successfully created new spot for user ${userId}`);
+});
+
+router.post("/:spotId/images", async (req, res) => {
+  const { user } = req;
+  const userId = user.dataValues.id;
+  const { url } = req.body;
+  const spot = await Spot.findByPk(req.params.spotId, { raw: true });
+  if (!spot) res.status(404).json("Spot does not exist");
+  if (userId === spot.ownerId) {
+    const newImage = await SpotImage.create({
+      url,
+      spotId: spot.id,
+    });
+    res.status(201);
+    res.json(newImage);
+  } else {
+    res.json("something went wrong");
+  }
 });
 
 module.exports = router;
