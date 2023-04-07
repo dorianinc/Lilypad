1fdc90b0ff2021a20a5d06f63fec939e164843f9
dorianinc <dorianrdba@gmail.com>
2023-04-07 10:05:52 -0700

    finished login basics

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,8 +33,14 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
-const initialState = { user: null };
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 
+const initialState = { user: null };
 const sessionReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
