cbf27976de241e2d01710e0af21f118ebcd160e3
dorianinc <dorianrdba@gmail.com>
2023-04-12 19:08:51 -0700

    round 7?!

--- frontend/src/components/SpotPage/index.js
+++ frontend/src/components/SpotPage/index.js
@@ -17,75 +17,77 @@ function SpotPage() {
     // };
   }, []);
 
-  const spot = useSelector((state) => state.spots)[spotId];
-  console.log("spot 👉", spot)
-  const reviewsObj = useSelector((state) => state.reviews);
-  const reviews = Object.values(reviewsObj);
-  console.log("reviews 👉", reviews)
-  if (!spot || !spot.Owner || !spot.SpotImages) return <>Hello World</>;
-  
-  console.log("fsadffsaf", spot.SpotImages)
-  const previewImage = spot.SpotImages.find((image) => image.preview === 1);
-  console.log("previewImage 👉", previewImage)
-  const images = spot.SpotImages.filter((image) => image.id !== previewImage.id);
-  console.log("images 👉", images)
+  return (<h1>Hello World</h1>)
 
-  return (
-    <div class="mainContainer spots">
-      <h1>{spot.name}</h1>
-      <h2>
-        {spot.city}, {spot.state} {" - "} {spot.country}
-      </h2>
-      <div id="imagesContainer">
-        <div class="boxes" id="box-1">
-          <img id="previewImage" alt="preview" src={previewImage.url} />
-        </div>
-        {images.map((image) => (
-          <div class="boxes" id={`box-${image.id}`}>
-            <img src={image.url} />
-          </div>
-        ))}
-      </div>
-      <div id="spotMenu">
-        <div id="spotInfo">
-          <h2>
-            Hosted By: {spot.Owner.firstName} {spot.Owner.lastName}
-          </h2>
-          <p id="spotDescription">{spot.description}</p>
-        </div>
-        <div id="reserveSection">
-          <div id="priceAndRating">
-            <p id="spotPrice">
-              <span>${spot.price.toFixed(2)}</span> night
-            </p>
-            <p id="spotRating">
-              <i class="fa-solid fa-star" />
-              {/* {spot.avgStarRating ? " " + spot.avgStarRating.toFixed(2) : "New"} */}
-              {` | ${spot.numReviews} reviews`}
-            </p>
-          </div>
-          <button id="reserveButton" onClick={() => alert("Feature Coming Soon!")}>
-            Reserve
-          </button>
-        </div>
-      </div>
-      <hr />
-      <div className="reviewsContainer">
-        <h2>
-          <i class="fa-solid fa-star" />
-          {spot.avgStarRating ? " " + spot.avgStarRating.toFixed(2) : "New"}
-          {" | "}{spot.numReviews === 1 ? `${spot.numReviews} reviews` : `${spot.numReviews} review`}
-        </h2>
-        {reviews.map(review => (
-        <div className="reviewStatement">
-          <h3>{review.User.firstName}{" "}{review.User.lastName}</h3>
-          <h3 style={{ color: "lightgray" }}>Jan 2023</h3>
-          <p>{review.review}</p>
-        </div>
-        ))}
-      </div>
-    </div>
-  );
+  // const spot = useSelector((state) => state.spots)[spotId];
+  // console.log("spot 👉", spot)
+  // const reviewsObj = useSelector((state) => state.reviews);
+  // const reviews = Object.values(reviewsObj);
+  // console.log("reviews 👉", reviews)
+  // if (!spot || !spot.Owner || !spot.SpotImages) return <>Hello World</>;
+  
+  // console.log("fsadffsaf", spot.SpotImages)
+  // const previewImage = spot.SpotImages.find((image) => image.preview === 1);
+  // console.log("previewImage 👉", previewImage)
+  // const images = spot.SpotImages.filter((image) => image.id !== previewImage.id);
+  // console.log("images 👉", images)
+
+  // return (
+  //   <div class="mainContainer spots">
+  //     <h1>{spot.name}</h1>
+  //     <h2>
+  //       {spot.city}, {spot.state} {" - "} {spot.country}
+  //     </h2>
+  //     <div id="imagesContainer">
+  //       <div class="boxes" id="box-1">
+  //         <img id="previewImage" alt="preview" src={previewImage.url} />
+  //       </div>
+  //       {images.map((image) => (
+  //         <div class="boxes" id={`box-${image.id}`}>
+  //           <img src={image.url} />
+  //         </div>
+  //       ))}
+  //     </div>
+  //     <div id="spotMenu">
+  //       <div id="spotInfo">
+  //         <h2>
+  //           Hosted By: {spot.Owner.firstName} {spot.Owner.lastName}
+  //         </h2>
+  //         <p id="spotDescription">{spot.description}</p>
+  //       </div>
+  //       <div id="reserveSection">
+  //         <div id="priceAndRating">
+  //           <p id="spotPrice">
+  //             <span>${spot.price.toFixed(2)}</span> night
+  //           </p>
+  //           <p id="spotRating">
+  //             <i class="fa-solid fa-star" />
+  //             {/* {spot.avgStarRating ? " " + spot.avgStarRating.toFixed(2) : "New"} */}
+  //             {` | ${spot.numReviews} reviews`}
+  //           </p>
+  //         </div>
+  //         <button id="reserveButton" onClick={() => alert("Feature Coming Soon!")}>
+  //           Reserve
+  //         </button>
+  //       </div>
+  //     </div>
+  //     <hr />
+  //     <div className="reviewsContainer">
+  //       <h2>
+  //         <i class="fa-solid fa-star" />
+  //         {spot.avgStarRating ? " " + spot.avgStarRating.toFixed(2) : "New"}
+  //         {" | "}{spot.numReviews === 1 ? `${spot.numReviews} reviews` : `${spot.numReviews} review`}
+  //       </h2>
+  //       {reviews.map(review => (
+  //       <div className="reviewStatement">
+  //         <h3>{review.User.firstName}{" "}{review.User.lastName}</h3>
+  //         <h3 style={{ color: "lightgray" }}>Jan 2023</h3>
+  //         <p>{review.review}</p>
+  //       </div>
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default SpotPage;
