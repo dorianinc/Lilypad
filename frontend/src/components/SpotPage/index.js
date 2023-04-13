61300a78e14b02b2b5bb02124297f7216aa85ebe
dorianinc <dorianrdba@gmail.com>
2023-04-12 18:58:51 -0700

    round 5...

--- frontend/src/components/SpotPage/index.js
+++ frontend/src/components/SpotPage/index.js
@@ -22,15 +22,13 @@ function SpotPage() {
   const reviewsObj = useSelector((state) => state.reviews);
   const reviews = Object.values(reviewsObj);
   console.log("reviews 👉", reviews)
-  if (!spot || !spot.Owner) return <>Hello World</>;
-
-  // const previewImage = spot.SpotImages.find((image) => image.preview === 1);
-  // const images = spot.SpotImages.filter((image) => image.id !== previewImage.id);
-  // spot.SpotImages.forEach(image => {
-  //   console.log("image =>", image)
-  //   console.log("image id =>", image.id)
-  // });
-  // console.log("previewImage.id 👉", previewImage.id)
+  if (!spot || !spot.Owner || !spot.SpotImages) return <>Hello World</>;
+  
+  console.log("fsadffsaf", spot.SpotImages)
+  const previewImage = spot.SpotImages.find((image) => image.preview === 1);
+  console.log("previewImage 👉", previewImage)
+  const images = spot.SpotImages.filter((image) => image.id !== previewImage.id);
+  console.log("images 👉", images)
 
   return (
     <div class="mainContainer spots">
@@ -40,13 +38,13 @@ function SpotPage() {
       </h2>
       <div id="imagesContainer">
         <div class="boxes" id="box-1">
-          {/* <img id="previewImage" alt="preview" src={previewImage.url} /> */}
+          <img id="previewImage" alt="preview" src={previewImage.url} />
         </div>
-        {/* {images.map((image) => (
+        {images.map((image) => (
           <div class="boxes" id={`box-${image.id}`}>
             <img src={image.url} />
           </div>
-        ))} */}
+        ))}
       </div>
       <div id="spotMenu">
         <div id="spotInfo">
@@ -62,7 +60,7 @@ function SpotPage() {
             </p>
             <p id="spotRating">
               <i class="fa-solid fa-star" />
-              {spot.avgStarRating ? " " + spot.avgStarRating.toFixed(2) : "New"}
+              {/* {spot.avgStarRating ? " " + spot.avgStarRating.toFixed(2) : "New"} */}
               {` | ${spot.numReviews} reviews`}
             </p>
           </div>
