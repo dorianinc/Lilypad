388c31903203d24f9d6ca939ebe972fdf86f001f
dorianinc <dorianrdba@gmail.com>
2023-04-14 09:13:33 -0700

    changed logo and favicon

--- frontend/src/components/Navigation/LogoButton/LogoButton.js
+++ frontend/src/components/Navigation/LogoButton/LogoButton.js
@@ -1,12 +1,12 @@
-import logo from "../../../assets/images/logo.png"
+import logo from "../../../assets/images/airDnD.png"
 import "./LogoButton.css"
 
 function LogoButton() {
   
     return (
       <div className="logoContainer">
-          <img className ="logo" alt="airBnB" style={{height: 35, width: 40}} src={logo}/>
-          <p className="title">airbnb</p>
+          <img className ="logo" alt="airBnB"  src={logo}/>
+          <p className="title">airdnd</p>
       </div>
     );
   }
