7be4d710923d215cdb3381fb1ccd87340d0205bc
dorianinc <dorianrdba@gmail.com>
2023-06-24 22:41:26 -0700

    calendar now appears for bookings, missing some functionality

--- frontend/src/components/Bookings/index.js
+++ frontend/src/components/Bookings/index.js
@@ -1,19 +1,28 @@
+import { useState } from "react";
+import Calendar from "./Calendar/Calendar";
 import "./Bookings.css";
 
 const Bookings = () => {
+  const [startDate, setStartDate] = useState("Add Date");
+  const [endDate, setEndDate] = useState("Add Date");
+
   return (
     <div className="bookings-container">
       <div className="start-date-container">
         <p id="checkin-text">CHECK-IN</p>
-        <p id="start-date-text">Add Date</p>
+        <p id="start-date-text">{startDate}</p>
       </div>
       <div className="end-date-container">
         <p id="checkout-text">CHECKOUT</p>
-        <p id="end-date-text">Add Date</p>
+        <p id="end-date-text">{endDate}</p>
       </div>
       <div className="calendar-container">
         <div className="month-container">
-            
+          <div className="booking-summary-review">
+            <h2>4 Nights</h2>
+            <p>Aug 12, 2023 - Aug 17, 2023</p>
+          </div>
+          <Calendar />
         </div>
       </div>
     </div>
