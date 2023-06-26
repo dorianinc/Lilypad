15c823cb3699e0c60db19e7013e9620fe2153538
dorianinc <dorianrdba@gmail.com>
2023-06-26 02:54:13 -0700

    reformatted document

--- frontend/src/components/Bookings/Calendar/index.js
+++ frontend/src/components/Bookings/Calendar/index.js
@@ -7,7 +7,7 @@ const Calendar = ({ bookings }) => {
   const { onStartDate, setOnStartDate, booking, setBooking, setStartDate, setEndDate } =
     useCalendar();
 
-    const disabledDays = []
+  const disabledDays = [];
   const bookedDays = (day) => {
     // one days worth of milliseconds
     const oneDay = 86400000;
@@ -22,15 +22,15 @@ const Calendar = ({ bookings }) => {
         // we convert the dates into milliseconds then add one day
         let formattedStartDate = startDate.getTime() + oneDay;
         let formattedEndDate = endDate.getTime() + oneDay;
-        
+
         if (formattedDay >= formattedStartDate && formattedDay <= formattedEndDate) {
-          disabledDays.push(new Date(formattedDay))
-          return true
+          disabledDays.push(new Date(formattedDay));
+          return true;
         }
       }
     }
   };
-  
+
   const handleSelect = (ranges) => {
     const { selection } = ranges;
     const startDate = selection.startDate;
@@ -48,7 +48,7 @@ const Calendar = ({ bookings }) => {
   return (
     <DateRange
       months={2} // number of months thats are displayed
-      direction="horizontal" /// direction the months flow
+      direction="horizontal" // direction the months flow
       ranges={booking} // ** collects the start date and end date values **
       minDate={new Date()} // if less than minDate those days are disables
       disabledDay={bookedDays} // pushed the days that should be disabled into an array
@@ -56,7 +56,7 @@ const Calendar = ({ bookings }) => {
       onChange={handleSelect} // handles on change after every date is selected
       showDateDisplay={false} // hides the default start date - end date display
       focusedRange={onStartDate ? [0, 0] : [0, 1]} // [0,0] focuses the start date && [0,1] focuses the end date
-      showMonthAndYearPickers={false} /// hides the dropdown option for months
+      showMonthAndYearPickers={false} // hides the dropdown option for months
       fixedHeight={true} // keeps the calender a consistent height
     />
   );
